f621713606fff237a16badb83dad888e349c1333
gayathri924 <57364551+gayathri924@users.noreply.github.com>
2019-11-25 13:23:59 -0600

    Update lightEntity.js

--- lightEntity.js
+++ lightEntity.js
@@ -1,12 +1,21 @@
-(function () {
-    var clicked = false;
-    this.clickDownOnEntity = function (entityID, mouseEvent) {
-        if (clicked){
-            Entities.editEntity(entityID, { color: { red: 0, green: 255, blue: 255} });
-            clicked = false;
-        } else {
-            Entities.editEntity(entityID, { color: { red: 255, green: 255, blue: 0} });
-            clicked = true;
-        }
+(function() {
+    var MINIMUM_LIGHT_INTENSITY = 100.0;
+    var MAXIMUM_LIGHT_INTENSITY = 125.0;
+
+    // Return a random number between `low` (inclusive) and `high` (exclusive)
+    function randFloat(low, high) {
+        return low + Math.random() * (high - low);
+    }
+
+    var self = this;
+    this.preload = function(entityID) {
+        self.intervalID = Script.setInterval(function() {
+            Entities.editEntity(entityID, {
+                intensity: randFloat(MINIMUM_LIGHT_INTENSITY, MAXIMUM_LIGHT_INTENSITY)
+            });
+        }, 100);
     };
-})
+    this.unload = function() {
+        Script.clearInterval(self.intervalID);
+    }
+});
