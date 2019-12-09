6453454aebd4be5a59e9c018d51804024ff31a81
gayathri924 <57364551+gayathri924@users.noreply.github.com>
2019-12-09 11:15:22 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,10 +1,22 @@
-(function () {
-    var clicked = false;
-    this.clickDownOnEntity = function (entityID, mouseEvent) {
-        if (clicked){
-            print("hi");
-        } else {
-            
-        }
+var entityScript = (function () {
+    // Method is called for only this entity.
+    this.clickDownOnEntity = function (entityID, event) {
+        print("Entity : Clicked sphere ; " + event.type);
     };
-})
+});
+
+var sphereID = Entities.addEntity({
+    type: "Sphere",
+    position: Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV(MyAvatar.orientation, { x: 0, y: 0, z: -3 })),
+    script: "(" + entityScript + ")",  // Could host the script on a Web server instead.
+    lifetime: 300  // Delete after 5 minutes.
+});
+
+Entities.clickDownOnEntity.connect(function (entityID, event) {
+    // Signal is triggered for all entities.
+    if (entityID === sphereID) {
+        print("Interface : Clicked sphere ; " + event.type);
+    } else {
+        print("Interface : Clicked another entity ; " + event.type);
+    }
+});
