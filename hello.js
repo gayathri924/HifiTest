b0e519179ffc3f0b0ae75f4ddc8b0d03a7b74f71
gayathri924 <57364551+gayathri924@users.noreply.github.com>
2019-12-09 11:02:55 -0600

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+(function () {
+    var clicked = false;
+    this.clickDownOnEntity = function (entityID, mouseEvent) {
+        if (clicked){
+            console.log("Hello there!");
+            clicked = false;
+        } else {
+            
+        }
+    };
+})
