b408044f6bf634e564dc14c0ac456b7d68dbb934
gayathri924 <57364551+gayathri924@users.noreply.github.com>
2019-12-09 11:37:12 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,22 +1,12 @@
-var entityScript = (function () {
-    // Method is called for only this entity.
-    this.clickDownOnEntity = function (entityID, event) {
-        print("Entity : Clicked sphere ; " + event.type);
+(function () {
+    var clicked = false;
+    this.clickDownOnEntity = function (entityID, mouseEvent) {
+        if (clicked){
+            Entities.editEntity(entityID, { color: { red: 0, green: 255, blue: 255} });
+            clicked = false;
+        } else {
+            Entities.editEntity(entityID, { color: { red: 255, green: 255, blue: 0} });
+            clicked = true;
+        }
     };
-});
-
-var sphereID = Entities.addEntity({
-    type: "Sphere",
-    position: Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV(MyAvatar.orientation, { x: 0, y: 0, z: -3 })),
-    script: "(" + entityScript + ")",  // Could host the script on a Web server instead.
-    lifetime: 300  // Delete after 5 minutes.
-});
-
-Entities.clickDownOnEntity.connect(function (entityID, event) {
-    // Signal is triggered for all entities.
-    if (entityID === sphereID) {
-        print("Interface : Clicked sphere ; " + event.type);
-    } else {
-        print("Interface : Clicked another entity ; " + event.type);
-    }
-});
+})
